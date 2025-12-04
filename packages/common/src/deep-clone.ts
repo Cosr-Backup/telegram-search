00663d399eb7281dd99a0392d8a9f4c9932a6727
RainbowBird <git@luoling.moe>
2025-12-04 13:18:49 +0800

    fix(common): deep clone

--- packages/common/src/deep-clone.ts
+++ packages/common/src/deep-clone.ts
@@ -1,11 +1,9 @@
-import { circularStringify } from './circular-object'
-
 export function deepClone<T>(data?: T): T | undefined {
   if (!data)
     return data
 
   try {
-    return JSON.parse(circularStringify(data)) as T
+    return JSON.parse(JSON.stringify(data)) as T
   }
   catch (error) {
     throw new Error('Failed to deep clone data', { cause: error })
